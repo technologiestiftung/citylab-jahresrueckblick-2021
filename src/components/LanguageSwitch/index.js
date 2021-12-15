dc68c9f4a9c655eae525aeb15dd6a9703d8fe811
fdnklg <mail@fabiandinklage.com>
2021-12-15 10:27:02 +0000

    update

---
+++ src/components/LanguageSwitch/index.js
@@ -0,0 +1,11 @@
+import {ReactComponent as GlobeIcon} from 'assets/icons/globe.svg';
+
+function LanguageSwitch() {
+  return (
+    <div>
+      <GlobeIcon />
+    </div>
+  )
+}
+
+export default LanguageSwitch;
---
+++ src/sections/Header/index.js
@@ -0,0 +1,11 @@
+import LanguageSwitch from "../../components/LanguageSwitch";
+
+function Header() {
+  return (
+    <div>
+      <LanguageSwitch />
+    </div>
+  )
+}
+
+export default Header;
